9f31bc4a12d948034704b08935b6929388a08b7a
soo-hyeon99 <soohyuen@naver.com>
2024-09-04 11:50:32 +0900

    print 코드 수정

--- assignment_2.py
+++ assignment_2.py
@@ -8,6 +8,9 @@ students = {
 
 # 특정 학생의 이름을 입력 받아 점수를 출력
 name = "Bob"
+name2 = "Alice"
 score = students.get(name)
+score2 = students.get(name2)
 
 print(f"{name}의 점수는 {score}점입니다.")
+print(f"{name2}의 점수는 {score2}점입니다.")
